7d106d91cf4d1358878e5599f8c8e16d71b78cb7
GUILEERME <guilhermealtomare@gmail.com>
2025-04-18 00:19:35 -0300

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -16,27 +16,39 @@
                 <label for="nome"> Nome </label>
                 <input type="text" name="nome" id="nome" class="inputUsuario" required>
             </div>
+            <br>
+
             <div class="inputBox">
                 <label for="email"> Email </label>
                 <input type="email" name="email" id="email" class="inputUsuario" required>
             </div>
+            <br>
+
             <div class="inputBox">
                 <label for="dataNascimento"> Data de Nascimento </label>
-                <input type="date" name="dataNascimento"" id =" dataNascimento"" class="inputUsuario" required>
+                <input type="date" name="dataNascimento" id="dataNascimento" class="inputUsuario" required>
             </div>
+            <br>
+
             <div class="inputBox">
                 <label for="senha"> Senha </label>
-                <input type="password" name="senha"" id =" senha"" class="inputUsuario" required>
+                <input type="password" name="senha" id="senha" class="inputUsuario" required>
             </div>
+            <br>
             <div class="inputBox">
-                <label for="senha"> Confirmar Senha </label>
-                <input type="password" name="senha"" id =" senha"" class="inputUsuario" required>
+                <label for="confirmarSenha"> Confirmar Senha </label>
+                <input type="password" name="confirmarSenha" id="confirmarSenha" class="inputUsuario" required>
             </div>
+            <br>
+
             <p>Sexo</p>
-            <input type="radio" id="Masculino" name="sexo" value=" Masculino " required>
+            <input type="radio" id="Masculino" name="sexo" value="Masculino" required>
             <label for="Masculino"> Masculino </label>
-            <input type="radio" id="Feminino" name="sexo" value=" Feminino " required>
+            <input type="radio" id="Feminino" name="sexo" value="Feminino" required>
             <label for="Feminino"> Feminino </label>
+           
+            <br>
+            <input type="submit" name="submit" id="submit" value="Cadastrar">
         </form>
     </div>
 
