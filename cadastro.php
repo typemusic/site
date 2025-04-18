245e352519ee259f3257e6bc3924118f79f283b9
GUILEERME <guilhermealtomare@gmail.com>
2025-04-17 23:54:53 -0300

    Criar formulário de cadastro
    Não tem css, só coloquei os inputs mesmo e n tá funcionando :laughing:

--- cadastro.php
+++ cadastro.php
@@ -1,11 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Cadastrar</title>
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-    
+
+    <div class="box">
+        <form action="">
+            <div class="inputBox">
+                <label for="nome"> Nome </label>
+                <input type="text" name="nome" id="nome" class="inputUsuario" required>
+            </div>
+            <div class="inputBox">
+                <label for="email"> Email </label>
+                <input type="email" name="email" id="email" class="inputUsuario" required>
+            </div>
+            <div class="inputBox">
+                <label for="dataNascimento"> Data de Nascimento </label>
+                <input type="date" name="dataNascimento"" id =" dataNascimento"" class="inputUsuario" required>
+            </div>
+            <div class="inputBox">
+                <label for="senha"> Senha </label>
+                <input type="password" name="senha"" id =" senha"" class="inputUsuario" required>
+            </div>
+            <div class="inputBox">
+                <label for="senha"> Confirmar Senha </label>
+                <input type="password" name="senha"" id =" senha"" class="inputUsuario" required>
+            </div>
+            <p>Sexo</p>
+            <input type="radio" id="Masculino" name="sexo" value=" Masculino " required>
+            <label for="Masculino"> Masculino </label>
+            <input type="radio" id="Feminino" name="sexo" value=" Feminino " required>
+            <label for="Feminino"> Feminino </label>
+        </form>
+    </div>
+
 </body>
+
 </html>
