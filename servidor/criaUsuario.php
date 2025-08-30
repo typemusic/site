8e5eb292c66a1f546f401a832cdf1124d8137ddf
GUILEERME <guilhermealtomare@gmail.com>
2025-08-30 01:00:29 -0300

    tá funcionando a conexão

--- servidor/criaUsuario.php
+++ servidor/criaUsuario.php
@@ -1,37 +1,27 @@
 <?php
+require_once('config.php');
 
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $conexao = novaConexao();
 
-include 'config.php';
+    $senha_hash = password_hash($_POST['confirmarSenha'], PASSWORD_BCRYPT);
 
+    try {
+        $sql = "INSERT INTO tblUsuario (usrNome, usrEmail, usrDn, usrSenha, usrGenero) 
+                VALUES (:n, :e, :d, :s, :g)";
+        $stmt = $conexao->prepare($sql);
+        $stmt->bindValue(':n', $_POST['nome']);
+        $stmt->bindValue(':e', $_POST['email']);
+        $stmt->bindValue(':d', $_POST['dataNascimento']);
+        $stmt->bindValue(':s', $senha_hash);
+        $stmt->bindValue(':g', $_POST['sexo']);
+        $stmt->execute();
 
-// Para capturar os dados do formulário
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    $nome = $_POST['nome'];
-    $email = $_POST['email'];
-    $dataNascimento = $_POST['dataNascimento'];
-    $senha = $_POST['senha'];
-    $confirmarSenha = $_POST['confirmarSenha'];
-    $sexo = $_POST['sexo'];
+        // Redireciona para o index
+        header("Location: ../index.php");
+        exit();
+    } catch (PDOException $e) {
+        echo "Erro ao inserir registro: " . $e->getMessage();
+    }
 }
-
-
-// Hash
-$senha_hash = password_hash($confirmarSenha, PASSWORD_BCRYPT);
-
-// Para confirmar se as senhas batem
-if($senha !== $confirmarSenha){
-    $erro = " As senhas não coincidem";
-    exit();
-}
-
-$sql = "INSERT INTO tblUsuario (usrNome, usrEmail, usrDn, usrSenha, usrGenero) VALUES ( '$nome' , '$email' , '$dataNascimento' , '$senha_hash' , '$sexo')" ;
-
-if ($conn->query($sql) === TRUE) {
-    echo "Usuário criado com sucesso!";
-} else {
-    echo "Erro: " . $conn->error;
-}
-
-$conn->close();
-
-?>  
+?>
