cb403c8d6a538073ff02377903050580e32fd8e4
GUILEERME <guilhermealtomare@gmail.com>
2025-10-13 11:31:53 -0300

    SESSION

--- index.php
+++ index.php
@@ -1,52 +1,61 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <title>Embed Genius com PHP</title>
-</head>
-<body>
-
-<form method="POST" action="">
-    <label for="song_id">Digite o ID da música (Genius):</label>
-    <input type="text" id="song_id" name="song_id" placeholder="Ex: 11718636">
-    <button type="submit">Gerar Embed</button>
-</form>
-
-
-<button onclick="getLyrics()">Mostrar Letra</button>
-
-<div id="result"></div>
-
-<script>
-    // Função para extrair o texto das letras
-    function getLyrics() {
-        // Seleciona o elemento que contém as letras
-        var embedContent = document.querySelector(".rg_embed_body");
-
-        // Remove as tags <a> e mantém as quebras de linha <br>
-        var cleanedContent = embedContent.innerHTML.replace(/<a[^>]*>(.*?)<\/a>/g, '$1'); // Remove as tags <a> e mantém o texto dentro delas
-        cleanedContent = cleanedContent.replace(/<\/?a[^>]*>/g, ''); // Remover qualquer tag <a> restante
-
-        // Exibe o conteúdo sem as tags <a>, mas mantendo as quebras de linha
-        document.getElementById("result").innerHTML = cleanedContent;
-    }
-</script>
-
-
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Pegando o ID digitado pelo usuário e sanitizando
-    $song_id = htmlspecialchars($_POST['song_id']);
-
-    // Gerando o código com o ID inserido
-    echo '
-    <div id="rg_embed_link_' . $song_id . '" class="rg_embed_link" data-song-id="' . $song_id . '">
-        <script src="//genius.com/songs/' . $song_id . '/embed.js" crossorigin=""></script>
-    </div>';
-}
+session_start();
 ?>
 
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>TypeMusic</title>
+  <link rel="stylesheet" href="style.css" />
+  <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded" rel="stylesheet" />
+  <link rel="icon" href="img/favicon.png" type="image/png">
+</head>
+
+<body>
+  
+  <?php include "include/menu.php"; ?>
+  <?php include "include/searchBar.php"; ?>
+
+  <div class="main-content">
+
+    <img src="img/bannerIndex.jpg" class="banner">
+    <section class="carousels">
+      <h3>Recomendadas</h3>
+      <div class="carousel-container">
+        <button class="button-carousel prev">&#10094;</button>
+        <div class="carousel" id="carousel-recomendadas">
+          <!-- As músicas recomendadas serão carregadas aqui -->
+        </div>
+        <button class="button-carousel next">&#10095;</button>
+      </div>
+
+      <h3>Mais Ouvidas</h3>
+      <div class="carousel-container">
+        <button class="button-carousel prev">&#10094;</button>
+        <div class="carousel" id="carousel-mais-ouvidas">
+          <!-- As músicas mais ouvidas serão carregadas aqui -->
+        </div>
+        <button class="button-carousel next">&#10095;</button>
+      </div>
+
+      <h3>Mais Treinadas</h3>
+      <div class="carousel-container">
+        <button class="button-carousel prev">&#10094;</button>
+        <div class="carousel" id="carousel-mais-treinadas">
+          <!-- As músicas mais treinadas serão carregadas aqui -->
+        </div>
+        <button class="button-carousel next">&#10095;</button>
+      </div>
+    </section>
+    <?php include "include/footer.php"; ?>
+
+  </div>
+  <script src="js/index.js"></script>
 
 
 </body>
+
 </html>
